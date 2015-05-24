2366146524afb106633d684fec67acf8eb5c164c
Adam Miskiewicz <adam.skevy@mac.com>
2015-05-24 00:05:24 -0400

    downloading images

--- converter/converter.js
+++ converter/converter.js
@@ -2,13 +2,24 @@ import fs from 'fs';
 import path from 'path';
 import _ from 'lodash';
 import moment from 'moment';
+import request from 'request';
 
 // Get and parse the data
 import data from './data/reactjs-news.ghost.2015-05-24.json';
+
+const OLD_BASE_URL = 'https://reactjsnews.com';
+const NEW_BASE_URL = '/reactjsnews';
+
 const posts = data.db[0].data.posts;
 const users = _.indexBy(data.db[0].data.users, 'id');
 const tags = _.indexBy(data.db[0].data.tags, 'id');
 const postTags = data.db[0].data.posts_tags;
+
+const replaceAt = function(string, index, newStr, oldStrLen) {
+  return string.substr(0, index) + newStr + string.substr(index + oldStrLen);
+};
+
+const images = [];
 
 // Loop through posts
 // and generate new ones
@@ -28,6 +39,21 @@ _.each(_.filter(posts, { status: 'published' }), (postData) => {
       return tags[tagId].name;
     }).value();
 
+  // Find images
+  const imageRegex = /\!\[(.*)\]\((.*)\)/g;
+  let match = null;
+  while ((match = imageRegex.exec(postData.markdown)) !== null) {
+    const url = `${OLD_BASE_URL}${match[2]}`;
+    const basename = path.basename(url);
+
+    images.push([url, basename]);
+
+    const newUrl = `${NEW_BASE_URL}/img/${basename}`;
+
+    const newMatch = match[0].replace(/\!\[(.*)\]\((.*)\)/, `![$1](${newUrl})`);
+    postData.markdown = replaceAt(postData.markdown, match.index, newMatch, match[0].length);
+  }
+
   const newPost = `---
 layout: post
 title:  "${postData.title}"
@@ -44,5 +70,27 @@ ${postData.markdown}`;
     if (err) {
       throw err;
     }
+
+    console.log(`Wrote ${filename}`);
+  });
+});
+
+function downloadImage(uri, filename, cb) {
+  request.head(uri, function (err, res, body) {
+    if (err) {
+      throw err;
+    }
+
+    const r = request(uri).pipe(fs.createWriteStream(filename));
+
+    if (cb) {
+      r.on('close', cb);
+    }
+  });
+}
+
+_.each(images, ([url, basename]) => {
+  downloadImage(url, path.join(__dirname, '..', 'img', basename), () => {
+    console.log(`Downloaded ${url}.`);
   });
 });
